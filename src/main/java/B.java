5f9eee3551407972a0fa37b9c1f653810b5e059f
Karol Preiskorn <karol.preiskorn@gmail.com>
2020-03-13 11:05:49 +0100

    final assigment #1

--- src/main/java/B.java
+++ src/main/java/B.java
@@ -4,16 +4,12 @@
  * karol 2020-03-11
  **/
 
-class B extends A {
+public class B extends A {
 	/***
 	 * TODO: Write the header for the required method
 	 * @return
 	 * ***/
 	public String whoAmI() {
-		return "B";
-	}
-	
-	public String toString() {
 		return "B";
 	}
 }
